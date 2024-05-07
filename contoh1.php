e0b174df00bb23ada06d585bf0abd39887574812
nandaputriali <163952746+nandaputriali@users.noreply.github.com>
2024-05-07 08:55:47 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller 
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Nanda Putri
+              saya tinggal di ciputat 
+              olahraga yang saya sukai adalah tidur";
+    }
+}
